9a9b2e2574f37afa4db41f0920255455c245bcef
Ashanti Haring <ashanti.haring@gmail.com>
2019-05-03 21:18:36 +0200

    Finished all Animations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-$
+$(document).ready(() => {
+    new WOW().init();
+
+    $('.js-wp-2').waypoint(
+        direction => {
+            $('.js-wp-2').addClass('animated slideInUp');
+        },
+        {
+            offset: '30%'
+        }
+    );
+
+    $('.js-wp-3').waypoint(
+        direction => {
+            $('.js-wp-3').addClass('animated fadeIn');
+        },
+        {
+            offset: '70%'
+        }
+    );
+
+    $('.iphone-btn')
+        .delay(2000)
+        .animate({ bottom: '+=-3' }, 200);
+    $('.iphone-btn')
+        .delay(300)
+        .animate({ top: '+=-3' }, 200);
+});
